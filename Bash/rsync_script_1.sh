adb40c6ab0a0230868e95d5fb5d36429ac5e80d6
captam3rica <mattwilsonsc@gmail.com>
2017-01-23 09:56:07 -0500

    Bash scripts

---
+++ Bash/rsync_script_1.sh
@@ -0,0 +1,26 @@
+###
+# rsync script to syncronize the DeployStudioServer files between two servers
+# From NetBoot to NetBootm
+#
+# Matt Wilson
+# Kennesaw State University
+# November 29, 2016
+#
+# -i - output a change summary at the end of the sync
+# -v - verbose output
+# -p - preserve permissions
+# -r - recursively copy items
+# -u - skip files that are newer on the receiver
+# -c - skip based on checksum
+# -t - preserve times
+#
+###
+
+SOURCE="/Volumes/DSRepo"
+DESTINATION="/Volumes/DSR/images/"
+DESTSERVER=[Remote server]
+# EXCLUDE_FILES
+
+rsync -vvrucip --progress --itemize-changes --log-file=$SOURCE/rsync_netboot.log $SOURCE $USER@$DESTSERVER:$DESTINATION
+
+scp $SOURCE/rsync_netboot.log $USER@$PUSHTOSERVER:$DESTINATION
