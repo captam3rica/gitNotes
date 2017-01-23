275cd485d8321485ee4f6499665400c240c137f7
captam3rica <mattwilsonsc@gmail.com>
2017-01-23 10:45:52 -0500

    Moved bash scripts

---
+++ Coding/Bash/rsync_script_1.sh
@@ -0,0 +1,29 @@
+###
+# rsync script to syncronize the DeployStudioServer files between two servers
+#
+#
+# Matt Wilson
+# Kennesaw State University
+# November 29, 2016
+#
+# -i - output a change summary at the end of the sync
+# -v - verbose output
+# -p - preserve permissions
+# -r - recursively copy items
+# -u - skip files that are newer on the receiver
+# -c - skip based on checksum
+# -t - preserve times
+#
+###
+
+SOURCE="/Volumes/DSRepo"
+DESTINATION="/Volumes/DSR/images/"
+DESTSERVER=[Remote server]
+DATE=`date "+%Y%m%d"`
+# EXCLUDE_FILES
+
+rsync -vvrucip --progress --itemize-changes --human-readable --log-file=$SOURCE/rsync_netboot_"$DATE".log $SOURCE $USER@$DESTSERVER:$DESTINATION
+
+scp $SOURCE/rsync_netboot.log $USER@$PUSHTOSERVER:$DESTINATION
+
+exit
