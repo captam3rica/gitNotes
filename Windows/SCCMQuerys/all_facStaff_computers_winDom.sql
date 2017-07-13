c220ed8fbdb1efd9487fc5cb59e6647d35d50800
captam3rica <mattwilsonsc@gmail.com>
2017-07-12 21:03:33 -0400

    Format editsf

--- Windows/SCCMQuerys/all_facStaff_computers_winDom.sql
+++ Windows/SCCMQuerys/all_facStaff_computers_winDom.sql
@@ -1,1 +1,7 @@
-select distinct SMS_R_System.Name, SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.OperatingSystemNameandVersion, SMS_R_System.Client, SMS_R_System.SMSUniqueIdentifier from  SMS_R_System where SMS_R_System.Name like "%KSUP%"
+select distinct SMS_R_System.Name, 
+SMS_R_System.ResourceDomainORWorkgroup, 
+SMS_R_System.OperatingSystemNameandVersion, 
+SMS_R_System.Client, 
+SMS_R_System.SMSUniqueIdentifier 
+from  SMS_R_System 
+where SMS_R_System.Name like "%KSUP%"
