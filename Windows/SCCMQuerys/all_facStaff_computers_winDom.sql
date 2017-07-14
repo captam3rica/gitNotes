8c1a4260b1fbbf4f3c35e36746cea662245f4bb1
captam3rica <mattwilsonsc@gmail.com>
2017-07-14 04:50:43 -0400

    WQL query updates
    updates

--- Windows/SCCMQuerys/all_facStaff_computers_winDom.sql
+++ Windows/SCCMQuerys/all_facStaff_computers_winDom.sql
@@ -1,1 +1,11 @@
-select distinct SMS_R_System.Name, SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.OperatingSystemNameandVersion, SMS_R_System.Client, SMS_R_System.SMSUniqueIdentifier from  SMS_R_System where SMS_R_System.Name like "%KSUP%"
+select distinct SMS_R_System.Name,
+SMS_R_System.ResourceDomainORWorkgroup,
+SMS_R_System.OperatingSystemNameandVersion,
+SMS_R_System.Client,
+SMS_R_System.ClientType,
+SMS_R_System.LastLogonUserName,
+SMS_R_System.IPAddresses, 
+SMS_R_System.SMSUniqueIdentifier,
+SMS_R_System.ResourceId
+from  SMS_R_System
+where SMS_R_System.Name like "%KSUP%"
--- Windows/SCCMQuerys/computers_reporting_last_90_days.sql
+++ Windows/SCCMQuerys/computers_reporting_last_90_days.sql
@@ -1,1 +1,13 @@
-select distinct SMS_R_System.Name, SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.OperatingSystemNameandVersion, SMS_R_System.Client, SMS_G_System_WORKSTATION_STATUS.LastHardwareScan, SMS_R_System.ResourceId from  SMS_R_System inner join SMS_G_System_WORKSTATION_STATUS on SMS_G_System_WORKSTATION_STATUS.ResourceID = SMS_R_System.ResourceId where SMS_G_System_WORKSTATION_STATUS.LastHardwareScan >= DateAdd(day, -90,GetDate()) and SMS_R_System.Name like "%KSUP%"
+select distinct SMS_R_System.Name,
+SMS_R_System.ResourceDomainORWorkgroup, SMS_R_System.OperatingSystemNameandVersion,
+SMS_R_System.Client,
+SMS_R_System.ClientType,
+SMS_R_System.LastLogonUserName,
+SMS_R_System.IPAddresses,
+SMS_G_System_CH_ClientSummary.LastPolicyRequest,
+SMS_R_System.SMSUniqueIdentifier,
+SMS_R_System.ResourceId
+from SMS_R_System
+inner join SMS_G_System_CH_ClientSummary on SMS_G_System_CH_ClientSummary.ResourceID = SMS_R_System.ResourceId
+where SMS_G_System_CH_ClientSummary.LastPolicyRequest >= DateAdd(day, -90,GetDate())
+and SMS_R_System.Name like "%KSUP%"
